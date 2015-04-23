0f5de45605a1f99ffab630ac52fa8d89e4e4ead8
Thierry BM <beggars.banquet@gmail.com>
2015-04-23 02:32:32 -0400

    More work done on the normalization process, plus some automation on the matlab side

--- Opt.m
+++ Opt.m
@@ -1,3 +1,6 @@
+features = importdata('features.csv');
+returns = importdata('returns.csv');
+
 xs = features;
 rs = returns';
 
@@ -5,7 +8,9 @@ b1 = 0.2;
 b2 = 1;
 
 Rf = log(1.02)/252;
-rc = log(1.15)/252;
+%rc = log(1.50)/252;
+rc = Rf;
+lambda=0.5;
 
 [n p] = size(xs);
 
@@ -14,4 +19,6 @@ U = @(p) b1*(p-rc) + min(0, (b2-b1)*(p-rc));
 cvx_begin
     variable q(p)
     maximize(sum(U(rs'.*(xs*q) + Rf*(1-xs*q))))
+    subject to
+        norm(q,2) <= 1
 cvx_end
