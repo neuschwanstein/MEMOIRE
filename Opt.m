fe5809d59dfc2d380d220672efe5bb9280d4a98c
Thierry BM <beggars.banquet@gmail.com>
2015-04-27 01:54:07 -0400

    Refactored matlab code + some figs

--- Opt.m
+++ Opt.m
@@ -3,32 +3,22 @@ if (~exist('features','var'))
     rs = importdata('returns.csv');
 end
 
-% xs = xs(1:500,:);
-% rs = rs(1:500,:);
+bs = linspace(0,1,5);
+%bs = [0 1];
+returns = [];
 
-b1 = 1;
-b2 = 1;
+for b=bs
+    q = getOpt(0,b,xs,rs);
 
-Rf = log(1.02)/252;
-%rc = log(1.50)/252;
-rc = 0;
-lambda=0.5;
+    pfReturns = rs.*(xs*q) + Rf*(1-xs*q);
+    totalReturns = getCumulativeReturns(pfReturns);
+    returns = [returns totalReturns'];
+end
+% 
+% pfReturns = rs.*(xs*q) + Rf*(1-xs*q);
+% totalReturn = getCumulativeReturns(pfReturns);
+% totalRawReturns = getCumulativeReturns(rs);
 
-[n p] = size(xs);
-
-U2 = @(p) b1*(p-rc) + min(0, (b2-b1)*(p-rc));
-
-cvx_begin
-    variable q(p)
-    maximize(sum(U2(rs.*(xs*q) + Rf*(1-xs*q))))
-    subject to
-        norm(q,2) <= 1
-cvx_end
-
-pfReturns = rs.*(xs*q) + Rf*(1-xs*q);
-totalReturn = getCumulativeReturns(pfReturns);
-totalRawReturns = getCumulativeReturns(rs);
-
-semilogy([totalReturn' totalRawReturns'])
-xlim([0 length(totalReturn)]);
-legend('Portfolio Return', 'Raw Returns','Location','northwest');
+semilogy([totalRawReturns' returns])
+xlim([0 length(totalRawReturns)]);
+%legend('Portfolio Return', 'Raw Returns','Location','northwest');
--- getOpt.m
+++ getOpt.m
@@ -1,11 +1,13 @@
 function q = getOpt(rc,b2,xs,rs)
 
+Rf = log(1.02)/252;
+[n p] = size(xs);
 b1=1;
-U2 = @(p) b1*(p-rc) + min(0, (b2-b1)*(p-rc));
+U = @(p) b1*(p-rc) + min(0, (b2-b1)*(p-rc));
 
 cvx_begin
     variable q(p)
-    maximize(sum(U2(rs.*(xs*q) + Rf*(1-xs*q))))
+    maximize(sum(U(rs.*(xs*q) + Rf*(1-xs*q))))
     subject to
         norm(q,2) <= 1
 cvx_end
