85e2e3283c863a5c2fb14d2825a4cf2d2c387dd8
Thierry BM <beggars.banquet@gmail.com>
2015-10-26 18:50:30 -0400

    New version of the document is theoretically more sound.

--- Model/script.py
+++ Model/script.py
@@ -11,20 +11,20 @@ cfg.n,cfg.p = n,p
 t = create_data.create_rule()
 N = 2000
 
-ls = range(1,21)
+ls = range(1,22,5)
 results = np.empty(len(ls))
 
-# for i,l in enumerate(ls):
-#     qs = np.empty((cfg.p+1,N))
-#     print("lambda=",l)
+for i,l in enumerate(ls):
+    qs = np.empty((cfg.p+1,N))
+    print("lambda=",l)
 
-#     for j in range(N):
-#         print(j)
-#         X,r = create_data.create_data(t)
-#         q,_ = obj.solve_objective(X,r,l)
-#         qs[:,j] = q
+    for j in range(N):
+        print(j)
+        X,r = create_data.create_data(t)
+        q,_ = obj.solve_objective(X,r,l)
+        qs[:,j] = q
 
-#     cov_matrix = np.cov(qs)
-#     eigenvals, eigenvecs = np.linalg.eig(cov_matrix)
+    cov_matrix = np.cov(qs)
+    eigenvals, eigenvecs = np.linalg.eig(cov_matrix)
 
-#     results[i] = np.sort(eigenvals)[0]
+    results[i] = np.sort(eigenvals)[-1]
