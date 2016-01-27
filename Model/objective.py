5304efdab026d1fb19473e2fa877dffee0a827e2
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-26 20:22:38 -0500

    Intermediate commit before I move things seriously around.

--- Model/objective.py
+++ Model/objective.py
@@ -1,6 +1,7 @@
 # from cvxpy import *
 import cvxpy as cvx
 import numpy as np
+from numpy.linalg import norm
 
 class Utility:
     pass
@@ -31,8 +32,10 @@ class LinearUtility(Utility):
 
 class Config:
     # This class is meant to be accessed and changed by the `client.'
-    Rf = 0.0  
-    u = ExpUtility(0.8)
+    Rf = 0.0
+    # r_bar = Solve the problem here. Perhaps with a new Problem class or smthing like that.
+    u = LinearUtility(0.8)
+    λ = 1.0
 
 cfg = Config
 
@@ -42,11 +45,12 @@ def append_bias(xss):
     bias = np.ones(n)
     return np.c_[bias,xss]
 
-def solve_objective(X,r,l):
+def solve_objective(X,r):
     n,p = X.shape
 
     cost = lambda p,r: -cfg.u.cvx_utility(cvx.mul_elemwise(r,p) + (1-p)*cfg.Rf)
-    total_cost = lambda t: 1.0/n * cvx.sum_entries(cvx_cost(X*t,r)) + l*cvx.norm(t,2)**2
+    total_cost = lambda t: \
+                 1/n*cvx.sum_entries(cost(X*t,r)) + cfg.λ * cvx.norm(t)**2
 
     q = Variable(p)
     objective = Minimize(total_cost(q))
@@ -60,17 +64,8 @@ def solve_objective(X,r,l):
     
     return q.value.A1, problem.value
 
-def solve_unregularized_objective(X,r):
-    return solve_objective(X,r,l=0)
-
-def regularized_risk(X,r,q,l):
+def regularized_risk(X,r,q):
     n,_ = X.shape
-    cost = lambda p,r: -cfg.utility(r*p + (1-p)*cfg.Rf)
-    total_cost = 1/n * sum(cost(np.dot(X,q),r)) + l*sum(q**2)
-    return total_cost
-
-def risk(X,r,q):
-    n,_ = X.shape
-    cost = lambda p,r: -cfg.utility(r*p + (1-p)*cfg.Rf)
-    total_cost = 1/n * sum(cost(np.dot(X,q),r))
+    cost = lambda p,r: -cfg.u.utility(r*p + (1-p)*cfg.Rf)
+    total_cost = 1/n * sum(cost(np.dot(X,q),r)) + cfg.λ * norm(q)**2
     return total_cost
