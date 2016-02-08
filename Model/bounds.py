be40e21199b34bb1b57e0d1d7191bdc44d86b291
Thierry BM <thierry.bazier-matte@hec.ca>
2016-02-08 08:00:49 -0500

    Bounds clean-up

--- Model/bounds.py
+++ Model/bounds.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 import synth_data as synth
 from utility import *
-from problem import Problem
+import problem
 
 
 filename = 'fig/plot.pdf'
@@ -30,45 +30,19 @@ market = synth.GaussianMarket(r_distr,x_distrs)
 problem = problem.AbstractProblem(market,λ,u)
 X_true,r_true = market.sample(n_true)
 
+ns = [50,100,200]
 
-# def find_optimal_lambda(n_sample):
-#     median = lambda d: np.median(
-#     opt.minimize_scalar(median,
+for n in ns:
+    h = problem.risk_distribution(n)
+    density = gaussian_kde(h)
+    x = np.linspace(np.min(h),np.max(h),num=40)
+    plt.plot(x,density(x), label='$n=%d$' % n)
 
+plt.legend()
+title = 'Out of Sample Risk Histogram ($p={},\lambda={}$)'.format(p,λ)
+print(title)
+plt.title(title)
 
-def abs_risk_deviation(n_sample):
-    X,r = synth.market_sample(x_distrs,r_distr,cop,n_sample)
-    sample_problem = Problem(X,r,λ,u,Rf=0)
-    insample_cost = sample_problem.solve()
-    outsample_cost = sample_problem.outsample_risk(X_true,r_true)
-    return np.abs(insample_cost - outsample_cost)
-
-def get_outsample_distribution(n_sample):
-    outsample_distribution = \
-        pool.map(abs_risk_deviation, [n_sample]*n_experiments)
-    return outsample_distribution
-
-global pool
-
-if (__name__ == '__main__'):
-    ctx = mp.get_context('forkserver')
-    pool = ctx.Pool(ctx.cpu_count())
-    n_samples = [50,100,200]
-
-    for n_sample in n_samples:
-        risk_deviation = get_outsample_distribution(n_sample)
-        density = gaussian_kde(risk_deviation)
-        x = np.linspace(np.min(risk_deviation), np.max(risk_deviation), num=40)
-        plt.plot(x, density(x), label='$n=%d$'%n_sample)
-
-    plt.legend()
-    title = 'Out of Sample Risk Histogram ($p={},\lambda={}$)'.format(p,λ)
-    print(title)
-    plt.title(title)
-
-    plt.savefig(filename)
-    os.system('open ' + filename)
-    plt.clf()
-
-    pool.close()
-    pool.join()
+plt.savefig(filename)
+os.system('open ' + filename)
+plt.clf()
