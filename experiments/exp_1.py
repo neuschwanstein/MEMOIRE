1efd0cc4fa4f647c938617e3b81540564ee70ffb
Thierry BM <thierry.bazier-matte@hec.ca>
2016-11-08 16:02:31 -0500

    Big changes ahead:

--- experiments/exp_1.py
+++ experiments/exp_1.py
@@ -12,19 +12,17 @@ import cd.model.utility as ut
 plt.rcParams['font.family'] = 'serif'
 
 if __name__ == '__main__':
-    r = mkt.load(2007,2015)
-    newsmarket = r[['r']]
-    newsmarket = nm.NewsMarket(newsmarket)
+    newsmarket = nm.load_all()
+    newsmarket = newsmarket.during(True)
+    vol = mkt.load_vol(2007,2015)
+    newsmarket = newsmarket.join(vol,how='inner')
 
     analyzer = nma.NewsMarketAnalyzer(newsmarket,shuffle=False)
 
-    # u = ut.LinearUtility(0.6)
     u = ut.LinearPlateauUtility(1,0.8*max(newsmarket.r))
-    # u = ut.LinearPlateauUtility(0.8,0.8*max(newsmarket.r))
-    # λs = np.logspace(-3,-1,15)
-    λs = np.logspace(-5.5,-2,50)
+    λs = np.logspace(-5.5,-2,25)
 
-    cvs = analyzer.cross_val(λs,u)
+    cvs = analyzer.cross_val(λs,u,verbose=True)
 
     plt.plot(λs,cvs)
     plt.xscale('log')
@@ -34,3 +32,26 @@ if __name__ == '__main__':
     plt.legend(['In-sample CE','Out-sample CE'])
     plt.title('Cross validation of CE\n%s' % str(u))
     plt.show()
+    
+    # r = mkt.load(2007,2015)
+    # newsmarket = r[['r']]
+    # newsmarket = nm.NewsMarket(newsmarket)
+
+    # analyzer = nma.NewsMarketAnalyzer(newsmarket,shuffle=False)
+
+    # # u = ut.LinearUtility(0.6)
+    # u = ut.LinearPlateauUtility(1,0.8*max(newsmarket.r))
+    # # u = ut.LinearPlateauUtility(0.8,0.8*max(newsmarket.r))
+    # # λs = np.logspace(-3,-1,15)
+    # λs = np.logspace(-5.5,-2,50)
+
+    # cvs = analyzer.cross_val(λs,u)
+
+    # plt.plot(λs,cvs)
+    # plt.xscale('log')
+    # plt.axis(xmax=max(λs),xmin=min(λs))
+    # plt.xlabel('$\lambda$')
+    # plt.ylabel('Returns (%)')
+    # plt.legend(['In-sample CE','Out-sample CE'])
+    # plt.title('Cross validation of CE\n%s' % str(u))
+    # plt.show()
--- experiments/exp_3.py
+++ experiments/exp_3.py
@@ -1,3 +1,10 @@
-'''In this experiment, it would be interesting to try how the out of sample performance is
-really affected when dealing with an increasing sample size.'''
+import numpy as np
+import matplotlib.pyplot as plt
+import cvxpy as cvx
+
+import cd.datasets.market as mkt
+import cd.datasets.newsmarket as nm
+import cd.data.newsmarketanalyzer as nma
+
+import cd.model.utility as ut
 
